e3c4a03ec28e5df61171fc0c44a1a5a4f1ca9402
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-08 20:46:16 +0100

    css + ts

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,19 @@ import './style.css'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-
+  <div class="search active">
+    <input type="text" class="input" placeholder="Search..."/>
+    <button class="btn">
+      <i class="fas fa-search"></i>
+    </button>
+  </div>
 `
+const search = document.querySelector('.search') as HTMLDivElement
+const btn = document.querySelector('.btn') as HTMLButtonElement
+const input = document.querySelector('.input') as HTMLInputElement
+
+
+btn?.addEventListener('click', () => {
+  search?.classList.toggle('active')
+  input?.focus()
+})
